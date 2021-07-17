41088ac6f22fefde1a8ee82bfa8c4994479ca548
kotnikd3 <denis.kotnik@gmail.com>
2021-07-17 12:14:41 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="hero is-medium is-link">
+      <div class="hero-body">
+        <p class="title">
+          Sandra Postma
+        </p>
+        <p class="subtitle">
+          Welcome to my website
+        </p>
+      </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
