8d180f767e1354ab2fffdeee15054ceff06249e0
kotnikd3 <denis.kotnik@gmail.com>
2021-09-03 22:50:05 +0200

    First test design.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
-    <section class="hero is-medium is-link">
-      <div class="hero-body">
-        <p class="title">
-          Sandra Postma
-        </p>
-        <p class="subtitle">
-          Welcome to my website
-        </p>
-      </div>
-  </section>
+    <div>
+		<!-- Introduction -->
+		<section class="section">
+			<div class="container">
+				<div class="columns is-vcentered">
+					<!-- Column -->
+					<div class="column">
+						<section class="section">
+							<h1 class="title">Sandra Postma</h1>
+							<h2 class="subtitle">Ovej mij</h2>
+							<div class="content">
+								<p>Mijn naam is Sandra, lichaamsgericht therapeut in opleiding.</p>
+								<p>Ik geloof dat wij allemaal, buitengewoon mens zijn. Iedereen is op zijn of haar manier bijzonder en geeft een unieke bijdrage aan het leven. Daarnaast, zijn wij mensen heel gewoon en dealen we met varianten van dezelfde issues. Mens zijn gaat er voor mij over dat alles er mag zijn: van verdriet, boosheid, angst tot speelsheid, blijdschap en vreugde én om daarbij de pijn en het ongemak te ontvangen over dat het leven niet altijd is en gaat zo als we willen. De uitdaging is om alle delen van onszelf een plek te geven en om ze samen te laten werken voor een leven dat ons vervult.</p>
+								<p>Ik heb veel reizen gemaakt naar verre bestemmingen, uit interesse, als antropoloog en deels omdat ik op de vlucht was voor mezelf. Langere tijd is de focus nu de reis in mijzelf en deze reis vervult mij het meest. Om volop in het leven aanwezig te kunnen zijn heb ik ontdekt dat het belangrijkste voor mij is om thuis te zijn bij mezelf, verbonden te zijn met mijn lichaam, mijn emoties te omarmen en een zinvol leven te leiden. Dit geeft mij de basis om de ander te ontmoeten en in de wereld te zijn. Tot mijn standaarduitrusting behoort zelfcompassie, lichaamswerk, meditatie, zelfexpressie, een visie en een bedding waardoor ik leer, ontdek, val en weer op sta.</p>
+								<p>Wil je in beweging komen en op reis in jezelf? JA? Vervolg,</p>
+								<p>Ik kan je gidsen. Waarbij? Denk aan:</p>
+								<ul>
+									<li>je wilt leven vanuit je lijf en minder vanuit je hoofd;</li>
+									<li>meer in contact komen met je emoties;</li>
+									<li>ontdekken wie en waarvoor je hier bent;</li>
+									<li>zicht krijgen op de verschillende delen in jezelf;</li>
+									<li>de volheid van het leven ervaren;</li>
+									<li>blijer en vrijer zijn met jezelf. </li>
+								</ul>
+								<p>Ik geef proefsessies vanuit mijn opleiding lichaamsgerichte psychotherapie. Dit zijn sessies van ander halfuur en kosten <b>€25</b>. Wil je meer informatie, stuur me dan een e-mail.</p>
+							</div>
+						</section>
+				 	</div>
+					<!-- Column -->
+				 	<div class="column has-text-centered">
+						<img src="./assets/sandra_2.jpg" alt="Denis Kotnik">
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
+  	name: 'App'
 }
 </script>
