607e589955f863b393bc7b24b50b0b76e659541f
kotnikd3 <denis.kotnik@gmail.com>
2021-10-10 13:00:55 +0200

    Changed text under sessions

--- src/App.vue
+++ src/App.vue
@@ -124,7 +124,9 @@
 				<h4 class="title is-4">Sessies</h4>
 				<div class="content">
 					<p>Ik geef onder supervisie van Bodymind Opleidingen sessies in lichaamsgerichte psychotherapie. Een sessie duurt 60 tot 75 minuten en bied ik aan tegen gereduceerd tarief omdat ik in opleiding ben. Een sessie kost €25 en vindt plaats in Utrecht (Zuilen).</p>
-					<p>Je bent welkom voor een vrijblijvend intakegesprek om te kijken wat sessies voor jou kunnen betekenen. Een intakegesprek is meestal via (video)bellen. Voel je vrij om contact met me op te nemen als je vragen hebt of informatie wil. Via e-mail ben ik het beste bereikbaar.</p>
+					<p>Wil je graag een sessie doen? Dan ben je welkom voor een vrijblijvend intakegesprek om te kijken wat sessies voor jou kunnen betekenen. Een intakegesprek kan live of via (video) bellen.</p>
+					<p>Wil je vragen of meer informatie? Voel je vrij om contact met me op te nemen.</p>
+					<p>Ik ben als student lid aangesloten bij de Stichting Beroepsorganisatie voor Lichaamsgeoriënteerde (Psycho)therapie (SBLP). De sessies zijn <i>nog niet declarabel bij de zorgverzekering.</i></p>
 					<p><i>Op dit moment werk ik niet met mensen jonger dan 21 jaar of mensen met contra-indicaties.</i></p>
 				</div>
 			</div>
