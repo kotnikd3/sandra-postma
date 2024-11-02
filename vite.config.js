909b5778a4bd192c61d6f11c3c96e4c8838ef957
kotnikd3 <denis.kotnik@gmail.com>
2024-11-02 12:39:01 +0100

    Try

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/sandra-postma/",
+    base: "/kotnikd3.github.io/",
     plugins: [
         vue(),
         vueDevTools(),
