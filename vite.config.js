de3d587b6afed386e6419c03abf866fdb5113411
kotnikd3 <denis.kotnik@gmail.com>
2024-11-02 12:41:08 +0100

    Try

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/kotnikd3.github.io/",
     plugins: [
         vue(),
         vueDevTools(),
