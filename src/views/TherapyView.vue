58a477e13867e55c44d4d9d104274206985f5a0c
kotnikd3 <denis.kotnik@gmail.com>
2025-03-24 10:43:34 +0100

    Implement TherapyView

--- src/views/TherapyView.vue
+++ src/views/TherapyView.vue
@@ -1,9 +1,35 @@
 <template>
-    <div class="section">
-        <div class="container">
-            Therapy View
+    <!-- Hero -->
+    <section class="hero is-medium has-bg-img">
+        <div class="hero-body">
+            <p class="title has-text-warning" v-text="t('therapyTitle')"></p>
         </div>
-    </div>
+    </section>
+    <!-- Therapy -->
+    <section class="section">
+        <div class="container">
+            <h4 class="title is-4" v-text="t('therapyTitleOne')"></h4>
+            <div class="content" v-text="t('therapyTextOne')"></div>
+            <div class="content has-text-centered">
+                <a class="button is-warning" v-text="t('informationLinkText')" to="#more"></a>
+            </div>
+            <div class="content" v-text="t('therapyTextTwo')"></div>
+            <div class="content" v-text="t('therapyTextThree')"></div>
+            <div class="content has-text-centered">
+                <RouterLink class="button is-warning" v-text="t('contactPageButtonText')" to="/about"></RouterLink>
+            </div>
+            <h4 class="title is-4" v-text="t('therapyTitleTwo')"></h4>
+            <div class="content" v-text="t('therapyTextFour')"></div>
+            <div class="content has-text-centered">
+                <a class="button is-warning" v-text="t('informationLinkText')" to="#more"></a>
+            </div>
+            <div class="content" v-text="t('therapyTextFive')"></div>
+            <div class="content" v-text="t('therapyTextSix')"></div>
+            <div class="content" v-text="t('therapyTextSeven')"></div>
+            <h4 class="title is-4" v-text="t('therapyTitleThree')"></h4>
+            <div class="content" v-text="t('therapyTextEight')"></div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -17,3 +43,10 @@
         },
     }
 </script>
+
+<style scoped>
+    .has-bg-img { 
+        background: url('@/assets/mist.webp') center center; 
+        background-size:cover; 
+    }
+</style>
